95e02b45e6a59ada8caa0bf12671a68353036ff1
rahul bauri <kumarrahulbauri2000@gmail.com>
2026-04-13 16:53:16 +0530

    added admin routers

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const app = express();
 
+const port = 3000;
+
 const { userRouter } = require('./routes/user');
 const { courseRouter } = require('./routes/course');
+const { adminRouter } = require('./routes/admin');
 
-app.use('/user', userRouter);
-app.use('/course', courseRouter);
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/course', courseRouter);
+app.use('/api/v1/admin', adminRouter);
 
-app.listen(3000, () => {
-  console.log('server is running on port 3000');
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
 });
